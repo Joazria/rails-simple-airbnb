b86e230376a6fa33139ec66246cae6a5d3968572
JoA Azria <joa.azria@gmail.com>
2020-10-15 22:18:36 -0300

    index view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,13 +1,59 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
   def index
+    @flats = Flat.all
   end
 
+  # GET /articles/1
   def show
   end
 
+  # GET /articles/new
   def new
+    @flat = Flat.new
   end
 
+  # GET /articles/1/edit
   def edit
   end
+
+  # POST /articles
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to @flat, notice: 'A new flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: 'The flat was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @flat.destroy
+    redirect_to flats_url, notice: 'The flat was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_flat
+      @flat = Flat.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+puts "Creating Fake Flats"
+10.times do |i|
+  Flat.create(
+    name: Faker::Artist.name,
+    address: Faker::Address.street_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: [ 75, 100, 125].sample,
+    number_of_guests: rand(1..5)
+    )
+  puts "Finished! restaurant #{ i + 1} created"
+end
+
+
